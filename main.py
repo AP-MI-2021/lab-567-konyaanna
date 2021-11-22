a464a04a7343c7bd205728c6c7125f5816f31dd4
konyaanna <91841714+konyaanna@users.noreply.github.com>
2021-11-22 13:06:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from Tests.TestAll import runalltests
 from UI.Console import run_menu
+from UI.Console2 import run_menu_2
 
 
 def main():
     runalltests()
+    optiune = input("Old menu or New menu? (old/new): ")
+    while optiune != "old" and optiune != "new":
+        print("Optiune invalida! Reincercati cu old/new.")
+        optiune = input("Old menu or New menu? (old/new): ")
+    if optiune == "old":
+        run_menu([])
+    else:
+        run_menu_2([])
     run_menu([])
 
 
