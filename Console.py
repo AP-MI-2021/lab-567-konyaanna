c652677e0b93e5dfc2d0cf0ccccd63d7c9466f3b
konyaanna <91841714+konyaanna@users.noreply.github.com>
2021-12-08 23:35:15 +0200

    Add files via upload

--- Console.py
+++ Console.py
@@ -15,10 +15,11 @@ def print_menu():
     print("7. Ordonarea obiectelor crescător după prețul de achiziție")
     print("8. Afișarea sumelor prețurilor pentru fiecare locație")
     print("u. Undo")
+    print("r. Redo")
     print("x. Iesire")
 
 
-def ui_adauga_obiect(lista, undo_list):
+def ui_adauga_obiect(lista, undo_list, redo_list):
     try:
         ID = input("Dati ID-ul: ")
         nume = input("Dati numele: ")
@@ -27,22 +28,24 @@ def ui_adauga_obiect(lista, undo_list):
         locatie = input("Dati locatia:")
         rezultat = adauga_obiect(ID, nume, descriere, pret_achizitie, locatie, lista)
         undo_list.append(lista)
+        redo_list.clear()
         return rezultat
     except ValueError as ve:
         print("Eroare: ", ve)
         return lista
-def ui_sterge_obiect(lista, undo_list):
+def ui_sterge_obiect(lista, undo_list, redo_list):
     try:
         ID = input("Dati ID-ul obiectului care trebuie sters: ")
         rezultat = sterge_obiect(ID, lista)
         undo_list.append(lista)
+        redo_list.clear()
         return rezultat
     except ValueError as ve:
         print("Eroare: ", ve)
         return lista
 
 
-def ui_modifica_obiect(lista, undo_list):
+def ui_modifica_obiect(lista, undo_list, redo_list):
     try:
         ID = input("Dati ID-ul obiectului care trebuie modificat: ")
         nume = input("Dati noul nume: ")
@@ -51,6 +54,7 @@ def ui_modifica_obiect(lista, undo_list):
         locatie = input("Dati noua locatie:")
         rezultat = modifica_obiect(ID, nume, descriere, pret_achizitie, locatie, lista)
         undo_list.append(lista)
+        redo_list.clear()
         return rezultat
     except ValueError as ve:
         print("Eroare: ", ve)
@@ -62,19 +66,21 @@ def show_all(lista):
         print(to_string(obiect))
 
 
-def ui_mutare_obiect_locatie(lista, undo_list):
+def ui_mutare_obiect_locatie(lista, undo_list, redo_list):
     ID=input("Dati ID ul: ")
     locatie = input("Dati locatia: ")
     undo_list.append(lista)
+    redo_list.clear()
     return mutare_obiect_locatie(lista, ID, locatie)
 
 
-def ui_concatenare(lista, undo_list):
+def ui_concatenare(lista, undo_list, redo_list):
     try:
         string = input("Dati stringul: ")
         pret = float(input("Dati pretul: "))
         rezultat = concatenare(lista, string, pret)
         undo_list.append(lista)
+        redo_list.clear()
         return rezultat
     except ValueError as ve:
         print("Eroare! ", ve)
@@ -88,8 +94,9 @@ def ui_pret_max_locatie(lista):
             locatie_curenta=get_locatie(obiect)
 
 
-def ui_ordonare_crescator_pret(lista, undo_list):
+def ui_ordonare_crescator_pret(lista, undo_list, redo_list):
     undo_list.append(lista)
+    redo_list.clear()
     return ordonare_crescator_pret(lista)
 def ui_suma_pret_locatie(lista):
     """
@@ -106,32 +113,40 @@ def ui_suma_pret_locatie(lista):
 
 def run_menu(lista):
     undo_list = []
+    redo_list=[]
     while True:
         print_menu()
         optiune = input("Dati optiune: ")
         if optiune == "1":
-            lista = ui_adauga_obiect(lista, undo_list)
+            lista = ui_adauga_obiect(lista, undo_list, redo_list)
         elif optiune == "2":
-            lista = ui_sterge_obiect(lista, undo_list)
+            lista = ui_sterge_obiect(lista, undo_list, redo_list)
         elif optiune == "3":
-            lista = ui_modifica_obiect(lista, undo_list)
+            lista = ui_modifica_obiect(lista, undo_list, redo_list)
         elif optiune == "a":
             show_all(lista)
         elif optiune == "4":
-            lista = ui_mutare_obiect_locatie(lista, undo_list)
+            lista = ui_mutare_obiect_locatie(lista, undo_list, redo_list)
         elif optiune == "5":
-            lista = ui_concatenare(lista, undo_list)
+            lista = ui_concatenare(lista, undo_list, redo_list)
         elif optiune == "6":
             ui_pret_max_locatie(lista)
         elif optiune == "7":
-            lista = ui_ordonare_crescator_pret(lista, undo_list)
+            lista = ui_ordonare_crescator_pret(lista, undo_list, redo_list)
         elif optiune == "8":
             ui_suma_pret_locatie(lista)
         elif optiune == "u":
             if len(undo_list) > 0:
+                redo_list.append(lista)
                 lista = undo_list.pop()
             else:
                 print("Nu se poate face Undo")
+        elif optiune == "r":
+            if len(redo_list) > 0:
+                undo_list.append(lista)
+                lista = redo_list.pop()
+            else:
+                print("Nu se poate face Redo!")
         elif optiune == "x":
             break
         else:
--- Console2.py
+++ Console2.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from Logic.CRUD import adauga_obiect, sterge_obiect, modifica_obiect
 from Logic.Functionality import mutare_obiect_locatie, concatenare, ordonare_crescator_pret
 from UI.Console import show_all, ui_pret_max_locatie, ui_suma_pret_locatie
@@ -14,60 +16,68 @@ def print_menu2():
     print("7. Ordonarea obiectelor crescător după prețul de achiziție")
     print("8. Afișarea sumelor prețurilor pentru fiecare locație")
     print("undo . Undo")
+    print("redo: Redo")
     print("x. Iesire")
 
 
-def ui_adauga_obiect_2(ID, nume, descriere, pret_achizitie, locatie , lista, undo_list):
+def ui_adauga_obiect_2(ID, nume, descriere, pret_achizitie, locatie, lista, undo_list, redo_list):
     try:
         rezultat = adauga_obiect(ID, nume, descriere, pret_achizitie, locatie, lista)
         undo_list.append(lista)
+        redo_list.clear()
         return rezultat
     except ValueError as ve:
         print("Eroare: ", ve)
         return lista
 
 
-def ui_sterge_obiect_2(ID, lista, undo_list):
+def ui_sterge_obiect_2(ID, lista, undo_list, redo_list):
     try:
         rezultat = sterge_obiect(ID, lista)
         undo_list.append(lista)
+        redo_list.clear()
         return rezultat
     except ValueError as ve:
         print("Eroare: ", ve)
         return lista
 
 
-def ui_modifica_obiect_2(ID, nume, descriere, pret_achizitie, locatie, lista, undo_list):
+def ui_modifica_obiect_2(ID, nume, descriere, pret_achizitie, locatie, lista, undo_list, redo_list):
     try:
         rezultat = modifica_obiect(ID, nume, descriere, pret_achizitie, locatie, lista)
         undo_list.append(lista)
+        redo_list.clear()
         return rezultat
     except ValueError as ve:
         print("Eroare: ", ve)
         return lista
 
 
-def ui_mutare_obiect_locatie_2(ID, locatie, lista, undo_list):
+def ui_mutare_obiect_locatie_2(ID, locatie, lista, undo_list, redo_list):
     undo_list.append(lista)
+    redo_list.clear()
     return mutare_obiect_locatie(lista, ID, locatie)
 
 
-def ui_concatenare_2(string,pret,lista, undo_list):
+def ui_concatenare_2(string,pret,lista, undo_list, redo_list):
     try:
         rezultat = concatenare(lista, string, pret)
         undo_list.append(lista)
+        redo_list.clear()
         return rezultat
     except ValueError as ve:
         print("Eroare! ", ve)
 
 
-def ui_ordonare_crescator_pret_2(lista, undo_list):
+def ui_ordonare_crescator_pret_2(lista, undo_list, redo_list):
     undo_list.append(lista)
+    redo_list.clear()
     return ordonare_crescator_pret(lista)
 
 
 def run_menu_2(lista):
     undo_list = []
+    redo_list = []
     while True:
         cerinta = input("Dati cerinta: ")
         if cerinta == "stop":
@@ -83,37 +93,42 @@ def run_menu_2(lista):
                     descriere = c.split(",")[3]
                     pret_achizitie = float(c.split(",")[4])
                     locatie = c.split(",")[5]
-                    lista = ui_adauga_obiect_2(ID, nume, descriere, pret_achizitie, locatie, lista, undo_list)
+                    lista = ui_adauga_obiect_2(ID, nume, descriere, pret_achizitie, locatie, lista, undo_list, redo_list)
                 elif c[0:4] == "show":
                     show_all(lista)
                 elif c[0:6] == "delete":
                     ID = c.split(",")[1]
-                    lista = ui_sterge_obiect_2(ID, lista, undo_list)
+                    lista = ui_sterge_obiect_2(ID, lista, undo_list, redo_list)
                 elif c[0:6] == "modify":
                     ID = c.split(",")[1]
                     nume = c.split(",")[2]
                     descriere = c.split(",")[3]
                     pret_achizitie = float(c.split(",")[4])
                     locatie = c.split(",")[5]
-                    lista = ui_modifica_obiect_2(ID, nume, descriere, pret_achizitie, locatie, lista, undo_list)
+                    lista = ui_modifica_obiect_2(ID, nume, descriere, pret_achizitie, locatie, lista, undo_list, redo_list)
                 elif c[0]=="4":
                     ID = c.split(",")[1]
                     locatie = c.split(",")[2]
-                    ui_mutare_obiect_locatie_2(ID, locatie, lista, undo_list)
+                    ui_mutare_obiect_locatie_2(ID, locatie, lista, undo_list, redo_list)
                 elif c[0]=="5":
                     string = c.split(",")[1]
                     pret = float(c.split(",")[2])
-                    ui_concatenare_2(string, pret, lista, undo_list)
+                    ui_concatenare_2(string, pret, lista, undo_list, redo_list)
                 elif c[0]=="6":
                     ui_pret_max_locatie(lista)
                 elif c[0]=="7":
-                    lista=ui_ordonare_crescator_pret_2(lista, undo_list)
+                    lista=ui_ordonare_crescator_pret_2(lista, undo_list, redo_list)
                 elif c[0]=="8":
                     ui_suma_pret_locatie(lista)
                 elif c[0:4] == "undo":
                     if len(undo_list) > 0:
+                        redo_list.append(lista)
                         lista = undo_list.pop()
                     else:
                         print("Nu se poate face Undo")
+                elif c[0:4] == "redo":
+                    if len(redo_list) > 0:
+                        undo_list.append(deepcopy(lista))
+                        lista = redo_list.pop()
                 else:
                     print("Cerinta inexistenta! Mai incercati")
